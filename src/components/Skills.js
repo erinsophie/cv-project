86a4423c966dbc4e47b389561283c0a869a56171
Sophie Little <119007056+erinsophie@users.noreply.github.com>
2023-07-14 19:54:54 +0100

    Add skills section

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,11 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 
-function SkillsForm(props) {
+function SkillsForm({ addSkill }) {
+  const [currentSkill, setCurrentSkill] = useState("");
+
+  function handleSkillChange(event) {
+    setCurrentSkill(event.target.value);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    if (currentSkill !== "") {
+      addSkill(currentSkill);
+      setCurrentSkill("");
+    }
+  }
+
   return (
-    <form>
-      <input type="text" name="" value={""} />
+    <form className="skills-form" onSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder="Add 5 of your top skills"
+        value={currentSkill}
+        onChange={handleSkillChange}
+      />
+      <button className="add-btn">+</button>
     </form>
   );
 }
 
 export default SkillsForm;
+
+//name
+//onChange={}
+//value={}
--- src/components/Template.js
+++ src/components/Template.js
@@ -3,19 +3,37 @@ import SkillsForm from "./Skills";
 
 function Template() {
   const [showSkillsForm, setShowSkillsForm] = useState(false);
+  const [skills, setSkills] = useState([]);
 
   function handleClick() {
     setShowSkillsForm((prev) => !prev);
   }
 
+  function addSkill(skill) {
+    setSkills((prevSkills) => [...prevSkills, skill]);
+  }
+
   return (
     <div className="cv-template">
       <hr />
-      <p>SKILLS</p>
-      <button onClick={handleClick} className="add-btn">
-        Add skills +
-      </button>
-      {showSkillsForm && <SkillsForm />}
+
+      <div className="skills-section">
+        <div>
+          <p>SKILLS</p>
+          <button onClick={handleClick} className="open-form-btn">
+            Add skills +
+          </button>
+        </div>
+
+        <div className="skills-list">
+          <ul>
+            {skills.map((skill, index) => (
+              <li key={index}>{skill}</li>
+            ))}
+          </ul>
+          {showSkillsForm && <SkillsForm addSkill={addSkill} />}
+        </div>
+      </div>
     </div>
   );
 }
